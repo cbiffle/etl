66ba5d690c1f5d09759f3073a01690c439307260
Cliff L. Biffle <code@cliffle.com>
2014-03-03 14:16:33 -0800

    wip stm32f4xx dbg

---
+++ stm32f4xx/dbg.h
@@ -0,0 +1,20 @@
+#ifndef ETL_STM32F4XX_DBG_H_
+#define ETL_STM32F4XX_DBG_H_
+
+#include "etl/stm32f4xx/types.h"
+
+namespace etl {
+namespace stm32f4xx {
+
+struct Dbg {
+  #define ETL_BFF_DEFINITION_FILE etl/stm32f4xx/dbg.reg
+  #include "etl/biffield/generate.h"
+  #undef ETL_BFF_DEFINITION_FILE
+};
+
+extern Dbg dbg;
+
+}  // namespace stm32f4xx
+}  // namespace etl
+
+#endif  // ETL_STM32F4XX_DBG_H_
