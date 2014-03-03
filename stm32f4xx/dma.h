5b9521030e4319c53491b195db8221e2f4fd912e
Cliff L. Biffle <code@cliffle.com>
2014-03-03 14:18:39 -0800

    stm32f4xx dma

---
+++ stm32f4xx/dma.h
@@ -0,0 +1,49 @@
+#ifndef ETL_STM32F4XX_DMA_H_
+#define ETL_STM32F4XX_DMA_H_
+
+#include "etl/stm32f4xx/types.h"
+
+namespace etl {
+namespace stm32f4xx {
+
+struct Dma {
+  struct Stream {
+    enum class BurstSize {
+      single = 0b00,
+      incr_4_beat = 0b01,
+      incr_8_beat = 0b10,
+      incr_16_beat = 0b11,
+    };
+
+    enum class TransferSize {
+      byte = 0b00,
+      half_word = 0b01,
+      word = 0b10,
+    };
+
+    #define ETL_BFF_DEFINITION_FILE etl/stm32f4xx/dma_stream.reg
+    #include "etl/biffield/generate.h"
+    #undef ETL_BFF_DEFINITION_FILE
+  };
+
+  #define ETL_BFF_DEFINITION_FILE etl/stm32f4xx/dma.reg
+  #include "etl/biffield/generate.h"
+  #undef ETL_BFF_DEFINITION_FILE
+
+  Stream stream0;
+  Stream stream1;
+  Stream stream2;
+  Stream stream3;
+  Stream stream4;
+  Stream stream5;
+  Stream stream6;
+  Stream stream7;
+};
+
+extern Dma dma1;
+extern Dma dma2;
+
+}  // namespace stm32f4xx
+}  // namespace etl
+
+#endif  // ETL_STM32F4XX_DMA_H_
