084ccc2767295ff1c084a8d9da23ffb9bb3f4f74
Cliff L. Biffle <code@cliffle.com>
2014-03-03 11:15:13 -0800

    wip stm32f4xx buses

---
+++ stm32f4xx/ahb.h
@@ -0,0 +1,41 @@
+#ifndef ETL_STM32F4XX_AHB_H_
+#define ETL_STM32F4XX_AHB_H_
+
+#include "etl/stm32f4xx/types.h"
+
+namespace etl {
+namespace stm32f4xx {
+
+/*
+ * Names for AHB peripherals, so that we can reset/enable them generically.
+ * The enum values *happen* to contain some useful information, but you should
+ * not rely on this unless you're the RCC driver -- it may change.
+ */
+enum class AhbPeripheral : Word {
+  /*
+   * AHB1 peripherals
+   */
+  #define ETL_NONE (0xFF)
+  #define ETL_STM32F4XX_AHB_PERIPH(name, rst, en) \
+    name = (0 << 16) | (rst << 8) | (en << 0),
+  #include "etl/stm32f4xx/ahb1_peripherals.def"
+  #undef ETL_STM32F4XX_AHB_PERIPH
+  #undef ETL_NONE
+};
+
+inline unsigned get_bus_index(AhbPeripheral p) {
+  return static_cast<Byte>(static_cast<Word>(p) >> 16);
+}
+
+inline unsigned get_reset_index(AhbPeripheral p) {
+  return static_cast<Byte>(static_cast<Word>(p) >> 8);
+}
+
+inline unsigned get_enable_index(AhbPeripheral p) {
+  return static_cast<Byte>(p);
+}
+
+}  // namespace stm32f4xx
+}  // namespace etl
+
+#endif  // ETL_STM32F4XX_AHB_H_
---
+++ stm32f4xx/apb.h
@@ -0,0 +1,40 @@
+#ifndef ETL_STM32F4XX_APB_H_
+#define ETL_STM32F4XX_APB_H_
+
+#include "etl/stm32f4xx/types.h"
+
+namespace etl {
+namespace stm32f4xx {
+
+enum class ApbPeripheral : HalfWord {
+  /*
+   * APB1 peripherals
+   */
+  #define ETL_STM32F4XX_APB_PERIPH(slot, name) name = (0 << 8) | slot,
+  #define ETL_STM32F4XX_APB_RESERVED(slot) /* nothing */
+  #include "etl/stm32f4xx/apb1_peripherals.def"
+  #undef ETL_STM32F4XX_APB_RESERVED
+  #undef ETL_STM32F4XX_APB_PERIPH
+
+  /*
+   * APB2 peripherals
+   */
+  #define ETL_STM32F4XX_APB_PERIPH(slot, name) name = (1 << 8) | slot,
+  #define ETL_STM32F4XX_APB_RESERVED(slot) /* nothing */
+  #include "etl/stm32f4xx/apb2_peripherals.def"
+  #undef ETL_STM32F4XX_APB_RESERVED
+  #undef ETL_STM32F4XX_APB_PERIPH
+};
+
+inline unsigned get_bus_index(ApbPeripheral p) {
+  return (static_cast<Word>(p) >> 8) & 0xFF;
+}
+
+inline unsigned get_slot_index(ApbPeripheral p) {
+  return static_cast<Word>(p) & 0xFF;
+}
+
+}  // namespace stm32f4xx
+}  // namespace etl
+
+#endif  // ETL_STM32F4XX_APB_H_
