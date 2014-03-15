007e4433a3c5de8de1adef3ec086ed5184a72298
Cliff L. Biffle <code@cliffle.com>
2014-03-15 09:53:31 -0700

    wip type_traits

--- common/type_traits.h
+++ common/type_traits.h
@@ -25,6 +25,37 @@ struct RemoveReference<T &&> {
   typedef T type;
 };
 
+/*
+ * RemoveVolatile and RemoveConst strip qualifiers from a type.
+ */
+template <typename T>
+struct RemoveVolatile {
+  typedef T type;
+};
+
+template <typename T>
+struct RemoveVolatile<T volatile> {
+  typedef T type;
+};
+
+template <typename T>
+struct RemoveConst {
+  typedef T type;
+};
+
+template <typename T>
+struct RemoveConst<T const> {
+  typedef T type;
+};
+
+template <typename T>
+struct RemoveConstVolatile {
+ private:
+  typedef typename RemoveConst<T>::type no_const;
+ public:
+  typedef typename RemoveVolatile<no_const>::type type;
+};
+
 
 /*
  * Promoting values to types.
@@ -38,8 +69,63 @@ struct IntegralConstant {
   constexpr operator T() { return V; }
 };
 
-typedef IntegralConstant<bool, true>  TrueType;
-typedef IntegralConstant<bool, false> FalseType;
+template <bool V>
+struct BoolConstant : public IntegralConstant<bool, V> {};
+
+typedef BoolConstant<true>  TrueType;
+typedef BoolConstant<false> FalseType;
+
+/*
+ * Fundamental types.
+ */
+
+#define ETL_COMMON_SPECIALIZE(tmpl, type, value) \
+  template <> \
+  struct tmpl<type> : public BoolConstant<value> {}
+
+template <typename T>
+struct IsUnqualifiedIntegral : public FalseType {};
+
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, bool, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, char, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, signed char, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, unsigned char, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, short, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, unsigned short, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, int, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, unsigned int, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, long, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, unsigned long, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, long long, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedIntegral, unsigned long long, true);
+
+template <typename T>
+struct IsIntegral
+  : public IsUnqualifiedIntegral<typename RemoveConstVolatile<T>::type> {};
+
+template <typename T>
+struct IsUnqualifiedFloatingPoint : public FalseType {};
+
+ETL_COMMON_SPECIALIZE(IsUnqualifiedFloatingPoint, float, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedFloatingPoint, double, true);
+ETL_COMMON_SPECIALIZE(IsUnqualifiedFloatingPoint, long double, true);
+
+template <typename T>
+struct IsFloatingPoint
+  : public IsUnqualifiedFloatingPoint<typename RemoveConstVolatile<T>::type> {};
+
+template <typename T>
+struct IsArithmetic
+  : public BoolConstant<IsIntegral<T>::value || IsFloatingPoint<T>::value> {};
+
+template <typename T>
+struct IsSigned
+  : public BoolConstant<IsFloatingPoint<T>::value
+                        || (IsIntegral<T>::value && (T(-1) < T(0)))> {};
+template <typename T>
+struct IsUnsigned
+  : public BoolConstant<IsArithmetic<T>::value && !IsSigned<T>::value> {};
+
 
 /*
  * Reasoning about type relationships.
