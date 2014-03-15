2e1e4fc2caabd4028b9e125ab91ce962ec108be5
Cliff L. Biffle <code@cliffle.com>
2014-03-15 09:30:52 -0700

    wip types

---
+++ common/types.h
@@ -0,0 +1,12 @@
+#ifndef ETL_COMMON_TYPES_H_
+#define ETL_COMMON_TYPES_H_
+
+namespace etl {
+namespace common {
+
+typedef decltype(sizeof(char(0))) Size;
+
+}  // namespace common
+}  // namespace etl
+
+#endif  // ETL_COMMON_TYPES_H_
