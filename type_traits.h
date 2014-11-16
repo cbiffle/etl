2559222cad28bd520ff371d404c7800a53f21ec5
Cliff L. Biffle <code@cliffle.com>
2014-11-16 10:06:51 -0800

    type_traits: add Invoke, ConditionalT.

--- type_traits.h
+++ type_traits.h
@@ -34,11 +34,88 @@ using BoolConstant = std::integral_constant<bool, V>;
  * This can be used to pass a placeholder for a type in a value context,
  * without any overhead that might be associated with creating an actual
  * instance of the type (or even a pointer).
+ *
+ * For compatibility, this exposes both std-style and etl-style names.
  */
 template <typename T>
 struct TypeConstant {
-  typedef T Type;
+  using Type = T;
+  using type = T;
 };
+
+
+/*******************************************************************************
+ * Converting between C++03-style member type metafunctions and C++11-style
+ * alias metafunctions.
+ *
+ * In general, the syntax "typename T::type" or "typename T::Type" can be
+ * replaced by "etl::Invoke<T>", regardless of the complexity of T.
+ *
+ * If both 'type' and 'Type' members are present, the uppercase one will be
+ * preferred.
+ */
+
+// Helper class for distinguishing std from etl conventions
+template <typename T>
+struct HasUppercaseTypeMember {
+private:
+  /*
+   * Idiom alert.
+   *
+   * We want to determine if T has a member type named Type.  If we try to
+   * reference it and it doesn't exist, we'll take a compiler error.
+   *
+   * But if we try to reference it in a template context and it's *ambiguous*,
+   * we get a pass.
+   *
+   * So, we introduce ambiguity by extending T and adding a Type member type.
+   *
+   * If T had a Type member already, the "No" overload of test below -- normally
+   * treated as more specific and a better overload match -- will be ambiguous
+   * and the compiler will disable it.
+   *
+   * If T has no Type member, we've added one in Derived, so the "No" overload
+   * will be unambiguously preferred.
+   *
+   * Here's hoping that some future version of C++ adds some kind of "syntactic
+   * validity predicates" that can test this more cleanly, such as the
+   * 'requires' blocks proposed in Concepts Lite.
+   */
+  using Yes = char[2];
+  using No = char[1];
+
+  struct Fallback { struct Type {}; };
+  struct Derived : T, Fallback {};
+
+  template <class U>
+  static No & test(typename U::Type *);
+
+  template <class U>
+  static Yes & test(U *);
+
+public:
+  static constexpr bool value = sizeof(test<Derived>(nullptr)) == sizeof(Yes);
+};
+
+template <typename T, bool = HasUppercaseTypeMember<T>::value>
+struct InvokeHelper : public TypeConstant<typename T::type> {};
+
+template <typename T>
+struct InvokeHelper<T, true> : public TypeConstant<typename T::Type> {};
+
+template <typename T>
+using Invoke = typename InvokeHelper<T>::Type;
+
+
+/*******************************************************************************
+ * Compile-time conditional behavior.
+ */
+
+/*
+ * ConditionalT is a backport of C++14's conditional_t.
+ */
+template <bool B, typename T, typename F>
+using ConditionalT = Invoke<std::conditional<B, T, F>>;
 
 
 /*******************************************************************************
