9b89816230a2e2c84fedd74d75ad55a3fbd0613a
Cliff L. Biffle <code@cliffle.com>
2014-12-05 09:43:07 -0800

    armv7m: add header with implicit CRT main prototype.

---
+++ armv7m/implicit_crt0.h
@@ -0,0 +1,10 @@
+#ifndef _ETL_ARMV7M_IMPLICIT_CRT0_H_INCLUDED
+#define _ETL_ARMV7M_IMPLICIT_CRT0_H_INCLUDED
+
+/*
+ * A prototype for the main() function, as such a thing is assumed in
+ * freestanding environments.
+ */
+extern "C" int main();
+
+#endif  // _ETL_ARMV7M_IMPLICIT_CRT0_H_INCLUDED
