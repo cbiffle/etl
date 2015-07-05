f300893adf2c9b45b53cf74a5eb02ba06e05382f
Cliff L. Biffle <code@cliffle.com>
2015-07-04 19:46:57 -0700

    error/result: fix constexpr bug, document.
    GCC 5.1 refused to accept this formulation of Result, and I agree with
    its interpretation of the spec.

--- error/result.h
+++ error/result.h
@@ -9,12 +9,23 @@
 namespace etl {
 namespace error {
 
+/*
+ * Tag type used to signify an error result.
+ */
 struct Left {};
 static constexpr Left left {};
 
+/*
+ * Tag type used to signify a success result.
+ */
 struct Right {};
 static constexpr Right right {};
 
+/*
+ * Return value from operations that may fail; represents a return code with an
+ * optional result value that is only constructed if the return code signifies
+ * success.
+ */
 template <typename E, typename V>
 class Result {
 public:
@@ -26,15 +37,15 @@ public:
   Result() = delete;
 
   constexpr Result(E const & error)
-    : _status((ETL_ASSERT_CE(EStrategy::is_bad(error)), error)) {}
+    : _status{(ETL_ASSERT_CE(EStrategy::is_bad(error)), error)} {}
 
   constexpr Result(Left const &, E const & error)
-    : _status((ETL_ASSERT_CE(EStrategy::is_bad(error)), error)) {}
+    : _status{(ETL_ASSERT_CE(EStrategy::is_bad(error)), error)} {}
 
   template <typename W>
   Result(Right const &, W && value)
-    : _status(EStrategy::success_value),
-      _value(etl::forward<W>(value)) {}
+    : _status{EStrategy::success_value},
+      _storage{etl::forward<W>(value)} {}
 
   constexpr bool is_error() const {
     return EStrategy::is_bad(_status);
@@ -50,14 +61,21 @@ public:
 
   V & ref() {
     ETL_ASSERT(!is_error());
-    return _value;
+    return _storage.object;
   }
 
 private:
   E _status;
-  union {
-    V _value;
-  };
+
+  union Storage {
+    struct {} dummy;
+    V object;
+
+    ETL_INLINE constexpr Storage() : dummy{} {}
+
+    template <typename W>
+    ETL_INLINE Storage(W && value) : object{forward<W>(value)} {}
+  } _storage;
 };
 
 template <typename E, typename V>
